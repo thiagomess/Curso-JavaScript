b94e01feb9a9f4a85b166ac83aa78f7fe6805c09
Thiago Gomes <thiagogomes19@hotmail.com>
2017-12-15 10:52:19 -0200

    Capitulo 4 completo

--- js/principal.js
+++ js/principal.js
@@ -42,10 +42,11 @@
   }
 
 }
-titulo.addEventListener("click", mostraMensagem);
-function mostraMensagem(){
-  console.log("Olá, eu fui clicado!");
-}
+// Adiciona mensagem no console ao clicar no titulo da pagina
+// titulo.addEventListener("click", mostraMensagem);
+// function mostraMensagem(){
+//   console.log("Olá, eu fui clicado!");
+// }
 
 // Função anonima.
 // titulo.addEventListener("click",function(){
@@ -53,6 +54,36 @@ function mostraMensagem(){
 // });
 
 var botaoAdicionar = document.querySelector('#adicionar-paciente');
-botaoAdicionar.addEventListener('click', function(){
-  alert("cliquei");
+botaoAdicionar.addEventListener('click', function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+
+  console.log(pacienteTr);
+
 });
