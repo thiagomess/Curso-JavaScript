0f81ac1b2b0726fe40df43b5f1cd3ca7d6768d2a
Thiago Gomes <thiagogomes19@hotmail.com>
2017-12-19 14:52:56 -0200

    Aula 7

---
+++ js/remover-paciente.js
@@ -0,0 +1,21 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("tbody");
+
+tabela.addEventListener("dblclick", function(event){
+
+  event.target.parentNode.classList.add("fadeOut");
+  setTimeout(function() {
+      event.target.parentNode.remove();
+  },1000);
+
+
+});
+
+
+// pacientes.forEach(function(paciente){
+//   paciente.addEventListener("dblclick", function(){
+//     this.remove();
+//   });
+//
+// });
