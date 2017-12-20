e31d9cc88c06b3ad3a3e04fc147045b04f071ed9
Thiago Gomes <thiagogomes19@hotmail.com>
2017-12-20 16:18:34 -0200

    aula 8

--- js/form.js
+++ js/form.js
@@ -41,11 +41,11 @@ function montaTr(paciente) {
   var pacienteTr = document.createElement("tr");
   pacienteTr.classList.add("paciente");
 
-  pacienteTr.appendChild(montaTd(paciente.nome, "#info-nome"));
-  pacienteTr.appendChild(montaTd(paciente.peso, "#info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
   pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-  pacienteTr.appendChild(montaTd(paciente.gordura, "#info-gordura"));
-  pacienteTr.appendChild(montaTd(paciente.imc, "#info-imc"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
   return pacienteTr;
 }
